997b9bc5aeec9cadae3f66f81513b101ff7c122f
VishalBurra27 <102515414+VishalBurra27@users.noreply.github.com>
2022-06-29 20:40:08 +0530

    better now
    home page not yet completed.login and signup are pretty good.

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,15 +1,21 @@
 
 import React, { useState } from "react";
+import "./Homepage.css"
+
 
 export default function Homepage() {
 
-    return(<div>
-        <h1>Homepage</h1>
+    return(<div className="home">
+        <h1>IITH ed-on</h1>
         <h2>Welcome to the EMS project.</h2>
-        Click on <a href="/login/1">Login</a> to continue. If you don't have an account, click on <a href="/signup/1">Signup</a> to create one.
-        
-        If you are an instructor, use these links instead: <p><a href="/login/0">Login</a></p> <p><a href="/signup/0">Signup</a></p>
-        
+        <p>
+            Please Login to continue
+        </p>
+        <a href="/login">
+            <button type="submit" width="100px"className="btn btn-primary btn-block mb-4">
+                Login</button>
+        </a>
+
         
         
     </div>);
--- frontend/src/components/SignupPage.jsx
+++ frontend/src/components/SignupPage.jsx
@@ -1,49 +1,121 @@
 
-import React, { useState } from "react";
+import { useState } from "react";
+import React from "react";
 import axios from "axios";
-import { useNavigate, useParams } from 'react-router-dom';
-
+import { Link, Route, Router, Routes, useNavigate } from 'react-router-dom';
+import "./SignupPage.css"
 
 const SignupPage = () => {
-    const history = useNavigate();
-    const { role } = useParams();
-    const [User, makeUser] = useState({
-        name:"",
-        email:"",
-        username:"",
-        password:"",
-        role: (role == 1? "Student" : "Instructor")
-    })
-    //const history = useNavigate();
+    // const [contact, setContact] = useState({
+    //     fname:"",
+    //     lname:"",
+    //     email:""
+    // });
+
+    // const [password, setPassword] = useState('');
+    // const history = useNavigate();
     
-    function updateUser(event){
-        event.preventDefault();
-        const val = event.target.value;
-        const n = event.target.name;
+    // function updateContact(event){
+    //     event.preventDefault();
+    //     const val = event.target.value;
+    //     const n = event.target.name;
 
-        makeUser(prev => ({...prev, [n]:val}))
-    }
+    //     setContact(prev => ({...prev, [n]:val})
+    //     )
+    // }
 
-    const saveUser = async (e) => {
-        e.preventDefault();
-        await axios.post('http://localhost:5000/user', User);
-        history.push("/");
-    }
+    // const saveProduct = async (e) => {
+    //     e.preventDefault();
+    //     await axios.post('localhost:5001/users', {
+    //         contact: contact,
+    //         password: password
+    //     });
+    //     history.push("/");
+    // }
 
     return(
-        <div>
-            <form action="/signup">
-                <h1> Dive right in. </h1>
-                <input name="name" onChange={updateUser} placeholder="Full Name." />
-                <input name="email" onChange={updateUser} placeholder="Email." />
-                <input name="username" onChange={updateUser} placeholder="Username." />
-                <input name="password" onChange={updateUser} type="password"/>
+        
+<section className="text-center text-lg-start">
 
-                <p><button onClick={e => saveUser(e) }><a href="/">Signup.</a></button></p>
 
+
+  <div className="container py-4">
+    <div className="row g-0 align-items-center">
+      <div className="col-lg-6 mb-5 mb-lg-0">
+        <div className="hicascading-right" style={{
+            
+            }}>
+          <div className="card-body p-5 shadow-5 text-center">
+            <h2 className="fw-bold mb-5">Sign up now</h2>
+            <form>
+
+              <div className="row">
+                <div className="col-md-6 mb-4">
+                  <div className="form-outline">
+                    <input type="text" id="form3Example1" className="form-control" />
+                    <label className="form-label" htmlFor="form3Example1">First name</label>
+                  </div>
+                </div>
+                <div className="col-md-6 mb-4">
+                  <div className="form-outline">
+                    <input type="text" id="form3Example2" className="form-control" />
+                    <label className="form-label" htmlFor="form3Example2">Last name</label>
+                  </div>
+                </div>
+              </div>
+
+              <div className="form-outline mb-4">
+                <input type="email" id="form3Example3" className="form-control" />
+                <label className="form-label" htmlFor="form3Example3">Email address</label>
+              </div>
+
+              <div className="form-outline mb-4">
+                <input type="password" id="form3Example4" className="form-control" />
+                <label className="form-label" htmlFor="form3Example4">Password</label>
+              </div>
+
+              
+
+              <button type="submit" className="btn btn-info btn-block mb-4">
+                Sign up
+              </button>
+
+
+              <div className="text-center">
+                <p>or sign up with:</p>
+                <button type="button" className="btn btn-link btn-floating mx-1">
+                  <i className="fab fa-facebook-f"></i>
+                </button>
+
+                <button type="button" className="btn btn-link btn-floating mx-1">
+                  <i className="fab fa-google"></i>
+                </button>
+              </div>
+              <div className="login">
+                <p>Already have an account?</p>
+                <a href="/login">
+                  <button type="button" className="btn btn-info" >Login</button>
+                </a>
+                
+                
+              </div>
             </form>
+          </div>
         </div>
+      </div>
+
+      <div className="col-lg-6 mb-5 mb-lg-0">
+      <img src="https://i.pinimg.com/736x/23/1e/23/231e23bbfaad9a0c7afc9b83eb510ee5.jpg" className="w-100 rounded-4 shadow-4"/>
+
+      </div>
+    </div>
+  </div>
+
+</section>
+
     );
 };
 
 export default SignupPage;
+
+
