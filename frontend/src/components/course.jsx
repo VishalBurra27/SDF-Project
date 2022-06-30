e7e876ecf6faae544d33a61397b4ecaf144db41b
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2022-06-30 17:31:03 +0530

    No actual change. Just tried out a few things which didn't work.

--- frontend/src/components/course.jsx
+++ frontend/src/components/course.jsx
@@ -1,17 +1,32 @@
 
 import axios from 'axios'
 import { useState } from 'react';
+import { useEffect } from 'react';
 
-async function getCourses(){
-    await axios.get('https://localhost:5000/courses');
-}
+
 const CourseList = (params) => {
+    async function getCourses(){
+        const res = await axios.get('https://localhost:5000/courses');
+        theCourses(res.data)
+    }
+    var [courses, theCourses] = useState(["No courses"]);
+    useEffect(() => {
+        getCourses();
+    }, []);
+    
     // const [out, setOut] = useState()
     // console.log(params);
     // setOut(getCourses.data);
+
+
+    //this be the first frontend-backend information 
     return(<div>
-        <h1>Out</h1>
-    </div>)
+    <h1>Out.</h1>
+    <ul>
+        {/* {courses.map((item, index) => {
+            return(<li> <h2>{item}</h2></li>)
+        })} */}
+    </ul></div>)
 }
 
 export default CourseList;
